7735a1dc513f2e536c24d52bcdd6a844912e84ba
kamiyang <github@jan-stockfisch.de>
2019-07-09 16:27:13 +0200

    [FEATURE] Add some descriptions for interface

--- typo3/sysext/felogin/Classes/Service/RecoveryServiceInterface.php
+++ typo3/sysext/felogin/Classes/Service/RecoveryServiceInterface.php
@@ -22,20 +22,26 @@ namespace TYPO3\CMS\Felogin\Service;
 interface RecoveryServiceInterface
 {
     /**
+     * Sends email to $emailAddress with recovery instructions
+     *
      * @param string $emailAddress
      * @return void
      */
     public function sendRecoveryEmail(string $emailAddress): void;
 
     /**
-     * @param string $hash
-     * @param string $passwordHash
+     * Change the password for an user based on $hash.
+     *
+     * @param string $hash The hash of the feUser that should be resolved.
+     * @param string $passwordHash The new password.
      */
     public function updatePasswordAndInvalidateHash(string $hash, string $passwordHash): void;
 
     /**
-     * @param string $hash
-     * @return bool
+     * Returns true if a user exists with $hash as `felogin_forgothash`, otherwise false.
+     *
+     * @param string $hash The hash of the feUser that should be check for existence.
+     * @return bool Either true or false based on the existence of the user.
      */
     public function existsUserWithHash(string $hash): bool;
 }
