59a0681286b2c71828bd69fa2c4d56ff3b61e198
hliebe <h.liebe@neusta.de>
2019-10-07 09:27:06 +0200

    [TASK] update redirectHandler tests

--- typo3/sysext/felogin/Tests/Unit/Redirect/RedirectHandlerTest.php
+++ typo3/sysext/felogin/Tests/Unit/Redirect/RedirectHandlerTest.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace TYPO3\CMS\Felogin\Tests\Unit\Controller;
 
@@ -22,16 +22,13 @@ use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Core\Authentication\LoginType;
 use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Context\UserAspect;
-use TYPO3\CMS\Core\Site\SiteFinder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Web\Request;
 use TYPO3\CMS\Felogin\Redirect\RedirectHandler;
 use TYPO3\CMS\Felogin\Redirect\RedirectModeHandler;
 use TYPO3\CMS\Felogin\Redirect\ServerRequestHandler;
-use TYPO3\CMS\Felogin\Validation\RedirectUrlValidator;
 use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
-use function array_merge;
 
 /**
  * Test case
@@ -46,11 +43,6 @@ class RedirectHandlerTest extends UnitTestCase
     protected $resetSingletonInstances = true;
 
     /**
-     * @var RedirectUrlValidator
-     */
-    protected $redirectUrlValidator;
-
-    /**
      * @var RedirectHandler
      */
     protected $subject;
@@ -59,11 +51,6 @@ class RedirectHandlerTest extends UnitTestCase
      * @var ServerRequestInterface
      */
     protected $typo3Request;
-
-    /**
-     * @var SiteFinder
-     */
-    protected $siteFinder;
 
     /**
      * @var ServerRequestHandler
@@ -79,17 +66,10 @@ class RedirectHandlerTest extends UnitTestCase
     {
         parent::setUp();
 
-        $this->redirectUrlValidator = $this->prophesize(RedirectUrlValidator::class);
-        $this->typo3Request = $this->prophesize(ServerRequestInterface::class);
-
         $this->serverRequestHandler = $this->prophesize(ServerRequestHandler::class);
         $this->redirectModeHandler = $this->prophesize(RedirectModeHandler::class);
 
-        GeneralUtility::addInstance(RedirectUrlValidator::class, $this->redirectUrlValidator->reveal());
-        GeneralUtility::addInstance(SiteFinder::class, $this->siteFinder->reveal());
-
         $GLOBALS['TSFE'] = $this->prophesize(TypoScriptFrontendController::class)->reveal();
-        $GLOBALS['TYPO3_REQUEST'] = $this->typo3Request->reveal();
 
         $this->subject = new RedirectHandler(
             $this->serverRequestHandler->reveal(),
@@ -115,7 +95,7 @@ class RedirectHandlerTest extends UnitTestCase
 
         $this->subject->init(LoginType::LOGOUT, $settings, $request->reveal());
 
-        $this->redirectModeHandler->redirectModeLogout()->willReturn();
+        $this->redirectModeHandler->redirectModeLogout()->willReturn('');
 
         self::assertEquals($expect, $this->subject->processRedirect());
     }
@@ -124,13 +104,6 @@ class RedirectHandlerTest extends UnitTestCase
     {
         yield 'empty string on empty redirect mode' => ['', ['redirectMode' => '']];
         yield 'empty string on redirect mode logout' => ['', ['redirectMode' => 'logout']];
-//        yield 'empty string on redirect mode logout' => [
-//            '',
-//            [
-//                'redirectMode' => 'logout',
-//                'redirectPageLogout' => 10
-//            ]
-//        ];
     }
 
     /**
@@ -147,21 +120,14 @@ class RedirectHandlerTest extends UnitTestCase
         array $body,
         bool $userLoggedIn
     ): void {
+        $loginType = '';
         $this->setUserLoggedIn($userLoggedIn);
-        $this->typo3Request
-            ->getParsedBody()
-            ->willReturn(
-                array_merge(
-                    ['logintype' => ''],
-                    $body
-                )
-            );
 
-        $this->redirectUrlValidator
-            ->isValid(Argument::type('string'))
-            ->willReturn(true);
+        $this->serverRequestHandler
+            ->getRedirectUrlRequestParam()
+            ->willReturn($body['return_url'] ?? '');
 
-        $this->subject->init($settings, $this->prophesize(Request::class)->reveal());
+        $this->subject->init($loginType, $settings, $this->prophesize(Request::class)->reveal());
 
         self::assertEquals($expected, $this->subject->getLogoutRedirectUrl());
     }
@@ -175,23 +141,11 @@ class RedirectHandlerTest extends UnitTestCase
             ['return_url' => 'https://dummy.url'],
             false
         ];
-        yield 'redirect mode getpost should return param redirect_url on empty return_url' => [
-            'https://dummy.url/2',
-            ['redirectMode' => 'getpost'],
-            ['return_url' => '', 'redirect_url' => 'https://dummy.url/2'],
-            false
-        ];
         yield 'redirect mode getpost,logout should return param return_url on not logged in user' => [
             'https://dummy.url/3',
             ['redirectMode' => 'getpost,logout'],
             ['return_url' => 'https://dummy.url/3'],
             false
-        ];
-        yield 'redirect mode logout should return empty url on logged in user and missing setting redirectPageLogout' => [
-            '',
-            ['redirectMode' => 'logout'],
-            [],
-            true
         ];
     }
 
@@ -201,99 +155,25 @@ class RedirectHandlerTest extends UnitTestCase
     public function getLogoutRedirectUrlShouldReturnAlternativeRedirectUrlForLoggedInUserAndRedirectPageLogoutSet(
     ): void
     {
-        $settings = ['redirectMode' => 'getpost,logout', 'redirectPageLogout' => 10];
+        $loginType = '';
+        $settings = ['redirectMode' => 'logout'];
         $this->setUserLoggedIn(true);
-        $this->typo3Request
-            ->getParsedBody()
-            ->willReturn(
-                [
-                    'logintype'  => '',
-                    'return_url' => 'https://dummy.url/'
-                ]
-            );
 
-        $this->redirectUrlValidator
-            ->isValid(Argument::type('string'))
-            ->willReturn(true);
+        $this->serverRequestHandler
+            ->getRedirectUrlRequestParam()
+            ->willReturn([]);
 
-        $this->uriBuilder->reset()->shouldBeCalled();
-        $this->uriBuilder->setTargetPageUid(10)->shouldBeCalled();
-        $this->uriBuilder->build()->willReturn('https://valid.url/');
+        $this->redirectModeHandler
+            ->redirectModeLogout()
+            ->willReturn('https://logout.url');
 
-        $this->subject->init($settings, $this->prophesize(Request::class)->reveal());
+        $this->redirectModeHandler
+            ->init(Argument::type('array'), Argument::type(Request::class))
+            ->shouldBeCalled();
 
-        self::assertEquals('https://valid.url/', $this->subject->getLogoutRedirectUrl());
-    }
+        $this->subject->init($loginType, $settings, $this->prophesize(Request::class)->reveal());
 
-    /**
-     * @test
-     * @dataProvider getLoginRedirectUrlDataProvider
-     * @param string $expected
-     * @param array $settings
-     * @param array $body
-     */
-    public function getLoginRedirectUrlShouldReturnAlternativeRedirectUrl(
-        string $expected,
-        array $settings,
-        array $body
-    ): void {
-        $this->typo3Request
-            ->getParsedBody()
-            ->willReturn(
-                array_merge(
-                    ['logintype' => ''],
-                    $body
-                )
-            );
-
-        $this->redirectUrlValidator
-            ->isValid(Argument::type('string'))
-            ->willReturn(true);
-
-        $this->subject->init($settings, $this->prophesize(Request::class)->reveal());
-
-        self::assertEquals($expected, $this->subject->getLoginRedirectUrl());
-    }
-
-    public function getLoginRedirectUrlDataProvider(): Generator
-    {
-        yield 'empty redirect mode should return empty returnUrl' => ['', ['redirectMode' => ''], [], false];
-        yield 'redirect mode getpost should return param return_url' => [
-            'https://dummy.url',
-            ['redirectMode' => 'getpost'],
-            ['return_url' => 'https://dummy.url']
-        ];
-        yield 'redirect mode login should return empty url on missing setting redirectPageLogin' => [
-            '',
-            ['redirectMode' => 'login'],
-            []
-        ];
-    }
-
-    /**
-     * @test
-     */
-    public function getLoginRedirectUrlShouldReturnRedirectUrlOnRedirectModeLoginAndValidRedirectPageLogin(): void
-    {
-        $settings = ['redirectMode' => 'getpost,login', 'redirectPageLogin' => 5];
-        $this->typo3Request
-            ->getParsedBody()
-            ->willReturn(
-                ['logintype' => '', 'return_url' => 'https://dummy.url/']
-            );
-
-        $this->redirectUrlValidator
-            ->isValid(Argument::type('string'))
-            ->willReturn(true);
-
-        $this->uriBuilder->reset()->shouldBeCalled();
-        $this->uriBuilder->setTargetPageUid(5)->shouldBeCalled();
-        $this->uriBuilder->setLinkAccessRestrictedPages(true)->shouldBeCalled();
-        $this->uriBuilder->build()->willReturn('https://valid.url/');
-
-        $this->subject->init($settings, $this->prophesize(Request::class)->reveal());
-
-        self::assertEquals('https://valid.url/', $this->subject->getLoginRedirectUrl());
+        self::assertEquals('https://logout.url', $this->subject->getLogoutRedirectUrl());
     }
 
     protected function setUserLoggedIn(bool $userLoggedIn): void
